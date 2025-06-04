3560a6f702922986c74b825a724cb6865b2eb800
skiees <kmwirigi05@gmail.com>
2025-06-04 09:44:22 +0300

    feat: add SQLAlchemy Base configuration

--- seed.py
+++ seed.py
@@ -4,3 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+class Company(Base):
+    __tablename__ = 'companies'
+    id = Column(Integer, primary_key=True)
