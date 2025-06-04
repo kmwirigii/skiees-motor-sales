eb8a158956c505e48571aa344f1d0fe11d5c1f5f
skiees <kmwirigi05@gmail.com>
2025-06-04 09:45:38 +0300

    feat: create Company model skeleton

--- seed.py
+++ seed.py
@@ -7,3 +7,10 @@ Base = declarative_base()
 class Company(Base):
     __tablename__ = 'companies'
     id = Column(Integer, primary_key=True)
+
+    name = Column(String)
+    founding_year = Column(Integer)
+
+    def __repr__(self):
+        return f'<Company {self.name}>'
+    
